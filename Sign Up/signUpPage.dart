9fecff24b84c53d52aa52453775eb3d555acb452
Amaan Ahmad <65362744+amaankahmad@users.noreply.github.com>
2021-01-12 15:40:35 +0000

    Add files via upload

---
+++ Sign Up/signUpPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final orange = const Color(0xFFF2BA49);
+    final green = const Color(0xFF04A489);
+    final blue = const Color(0xFF1E325C);
+    final white = const Color(0xFFF1FAEE);
+    return Scaffold(
+      backgroundColor: white,
+      body: Column(),
+    );
+  }
+}
