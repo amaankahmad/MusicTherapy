68a8ea5b1a6a2a90b642d39c0d06137b6355d322
Teo56 <77128181+Teo56@users.noreply.github.com>
2021-01-14 00:28:41 +0100

    Add files via upload

---
+++ ConfirmExerciseUploadPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ConfirmExerciseUploadPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
