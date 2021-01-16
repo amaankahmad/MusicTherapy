2019703af73fd93ee608d6c5fce5416381b09c69
Sam <77116216+s4mwiniarski@users.noreply.github.com>
2021-01-16 18:36:34 +0000

    Add files via upload

--- Start (Player)/PlayerStartPage.dart
+++ Start (Player)/PlayerStartPage.dart
@@ -1,3 +1,6 @@
+// sambo slice
+// 16th jan
+
 import 'package:flutter/material.dart';
 
 class PlayerStartPage extends StatelessWidget {
@@ -11,102 +14,150 @@ class PlayerStartPage extends StatelessWidget {
     final white = const Color(0xFFFFFBF2);
      return Scaffold(
        backgroundColor: white,
-       body: Column(
-         //crossAxisAlignment: CrossAxisAlignment.center,
-         //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-           children: <Widget>[
-             SizedBox(
-               height: height * 0.1,
-             ),
-
-             CircleAvatar(
-               radius: 70,
-               backgroundColor: orange,
-               child: CircleAvatar(
-                 radius: 60,
-                 backgroundImage: AssetImage("images/parrot.jpg"),
-               ),
-             ),
-             SizedBox(
-                  height: height * 0.1,
-                  ),
-             Center(
-               child: Container(
-                 child: Text(
-                   'Hello, Sam',
-                   style: TextStyle(
-                     //fontFamily: 'Museo',
-                     fontSize: height * 0.08,
-                     color: blue,
-                     fontWeight: FontWeight.w700,
-                   ),
-                 ),
-               ),
-             ),
-             SizedBox(
-               height: height * 0.1,
-             ),
-             Container(
-               width: width * 0.7,
-               height: height * 0.25,
+       body: Stack(
+         children: <Widget>[
+// --------- BACK BUTTON --------------
+           Positioned(
+             top: 15,
+             left: 40,
+             child: Container(
+               width: 50,
+               height: 50,
                child: Material(
-                 borderRadius: BorderRadius.circular(40),
-                 shadowColor: blue,
-                 color: orange,
+                 borderRadius: BorderRadius.circular(10000),
+                 shadowColor: const Color(0xFF1E325C),
                  elevation: 5,
                  child: GestureDetector(
                    onTap: () {},
-                   child: Center(
+                   child: ClipOval(
                      child: Padding(
-                       padding: const EdgeInsets.symmetric(vertical: 12),
-                       child: Text(
-                         'Start!',
-                         style: TextStyle(
-                           color: white,
-                           //fontFamily: 'Museo',
-                           fontSize: 60,
-                           //fontWeight: FontWeight.w700,
-                         ),
+                       padding: const EdgeInsets.only(
+                         top: 12,
+                         left: 10,
+                         right: 12,
+                         bottom: 12,
                        ),
+                       child: Image.asset('assets/images/arrow.jpeg',),
                      ),
                    ),
                  ),
                ),
              ),
-             SizedBox(
-               height: height * 0.04,
-             ),
-             Container(
-               width: width * 0.7,
-               child: Material(
-                 borderRadius: BorderRadius.circular(40),
-                 shadowColor: blue,
-                 color: white,
-                 elevation: 5,
-                 child: GestureDetector(
-                   onTap: () {},
-                   child: Center(
-                     child: Padding(
-                       padding: const EdgeInsets.symmetric(vertical: 12),
-                       child: Text(
-                         'Settings',
-                         style: TextStyle(
-                           color: green,
-                           //fontFamily: 'Museo',
-                           fontSize: 40,
-                           //fontWeight: FontWeight.w700,
+           ),
+// --------- BACK BUTTON --------------
+// --------- ERRYTING ELSE ------------
+           Column(
+             //crossAxisAlignment: CrossAxisAlignment.center,
+             //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+               children: <Widget>[
+// --------- SPACING CONTROL --------------
+                 SizedBox(
+                   height: height * 0.1,
+                 ),
+// --------- SPACING CONTROL --------------
+// --------- AVATAR DON -------------------
+                 CircleAvatar(
+                   radius: 70,
+                   backgroundColor: orange,
+                   child: CircleAvatar(
+                     radius: 60,
+                     backgroundImage: AssetImage("assets/images/parrot.jpg"),
+                   ),
+                 ),
+// --------- AVATAR DON -------------------
+// --------- SPACING CONTROL --------------
+                 SizedBox(
+                   height: height * 0.1,
+                 ),
+// --------- SPACING CONTROL --------------
+// --------- WELCOME TEXT -----------------
+                 Center(
+                   child: Container(
+                     child: Text(
+                       'Hello, Sam',
+                       style: TextStyle(
+                         //fontFamily: 'Museo',
+                         fontSize: height * 0.08,
+                         color: blue,
+                         fontWeight: FontWeight.w700,
+                       ),
+                     ),
+                   ),
+                 ),
+// --------- WELCOME TEXT -----------------
+// --------- SPACING CONTROL --------------
+                 SizedBox(
+                   height: height * 0.1,
+                 ),
+// --------- SPACING CONTROL --------------
+// --------- START BUTTON -----------------
+                 Container(
+                   width: width * 0.7,
+                   height: height * 0.25,
+                   child: Material(
+                     borderRadius: BorderRadius.circular(40),
+                     shadowColor: blue,
+                     color: orange,
+                     elevation: 5,
+                     child: GestureDetector(
+                       onTap: () {},
+                       child: Center(
+                         child: Padding(
+                           padding: const EdgeInsets.symmetric(vertical: 12),
+                           child: Text(
+                             'Start!',
+                             style: TextStyle(
+                               color: white,
+                               //fontFamily: 'Museo',
+                               fontSize: 60,
+                               //fontWeight: FontWeight.w700,
+                             ),
+                           ),
                          ),
                        ),
                      ),
                    ),
                  ),
-               ),
-             ),
+// --------- START BUTTON -----------------
+// --------- SPACING CONTROL --------------
+                 SizedBox(
+                   height: height * 0.04,
+                 ),
+// --------- SPACING CONTROL --------------
+// --------- SETTINGS BUTTON --------------
+                 Container(
+                   width: width * 0.7,
+                   child: Material(
+                     borderRadius: BorderRadius.circular(40),
+                     shadowColor: blue,
+                     color: white,
+                     elevation: 5,
+                     child: GestureDetector(
+                       onTap: () {},
+                       child: Center(
+                         child: Padding(
+                           padding: const EdgeInsets.symmetric(vertical: 12),
+                           child: Text(
+                             'Settings',
+                             style: TextStyle(
+                               color: green,
+                               //fontFamily: 'Museo',
+                               fontSize: 40,
+                               //fontWeight: FontWeight.w700,
+                             ),
+                           ),
+                         ),
+                       ),
+                     ),
+                   ),
+                 ),
+// --------- SETTINGS BUTTON --------------
 
-
-              ]
-          )
-      );
+               ]
+           )
+         ],
+       ),
+     );
   }
 }
 
