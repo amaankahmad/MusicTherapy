d6a4f6311f0de3a8d1739dac9fe0b9300df96eb9
Huang Jiaxin <67550368+hjx519@users.noreply.github.com>
2021-01-17 22:10:14 +0800

    Add back button

--- Create Exercises/CreateExercises.dart
+++ Create Exercises/CreateExercises.dart
@@ -1,21 +1,58 @@
 import 'package:flutter/material.dart';
 
-
 class CreateExercises extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-
+    final blue = const Color(0xFF1E325C);
     // TODO: implement build
-return MaterialApp(
+    return MaterialApp(
 home:Scaffold(
 backgroundColor: Colors.green[50],
-body: Center(
+body: 
+//-----------------------------------------
+// Back Button:
+   Stack(
+          children: <Widget>[      
+               
+              Positioned(
+                top: 15,
+                left: 40,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(10000),
+                    shadowColor: blue,
+                    elevation: 5,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: ClipOval(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            top: 12,
+                            left: 10,
+                            right: 12,
+                            bottom: 12,
+                          ),
+                          child: Image.asset(
+                            "assets/images/arrow.jpeg",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+//--------------------------------------------------------
+//Center+Column+Text...
+//Text "upload exercise video"
+Center(
 child: Column(
 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  //crossAxisAlignment: CrossAxisAlignment.baseline,
   children: [
+
         Text(
               'Upload\nExercise Video',
               textAlign: TextAlign.center,
@@ -23,11 +60,11 @@ mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               fontFamily: 'Museo',
               fontSize: height * 0.04,
               color: const Color(0xFF1E325C),
-              fontWeight: FontWeight.bold,
+              fontWeight: FontWeight.w700,
               ),
             ),
 //--------------------------------------------------
-//Container+Stack+Center+Column
+//Container+Stack+Center+SingleChildScrollView+Column
     Container(
     height: height*0.6,
     width:width*0.8,
@@ -39,11 +76,12 @@ mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       ),
     child:Stack(
       children: [
-        Center(         
-         child:Column
-         (children: [
+        Center(
+         child: SingleChildScrollView(         
+          child:Column
+          (children: [
            
- //Text: Enter Exercise Name:  
+ //Text "Enter Exercise Name:"
 SizedBox(height:height*0.04),//gap      
 SizedBox(
   width: width*0.7,
@@ -55,14 +93,14 @@ SizedBox(
     color: const Color(0xFF04A489),
     child:Padding(
     padding: const EdgeInsets.all(7),
-    child: Text(                              
-    '  Enter Exercise Name: ',
-     textAlign: TextAlign.justify,
-     style: TextStyle(
-     fontFamily: 'Museo',
-     fontSize: 15,
-     color: Colors.white,
-     fontWeight: FontWeight.bold),
+      child: Text(                              
+      '  Enter Exercise Name: ',
+      textAlign: TextAlign.justify,
+      style: TextStyle(
+      fontFamily: 'Museo',
+      fontSize: 15,
+      color: Colors.white,
+      fontWeight: FontWeight.bold),
       ),
     ),
   ),
@@ -84,8 +122,8 @@ SizedBox(
   ),
 ),
 
-//Text: Preview:
-SizedBox(height:height*0.06),//gap   
+//Text: "Upload video below:"
+SizedBox(height:height*0.05),//gap   
 SizedBox(
   width: width*0.7,
   height: 40,
@@ -97,7 +135,7 @@ SizedBox(
     child:Padding(
     padding: const EdgeInsets.all(7),
     child: Text(                              
-    '  Preview: ',
+    '  Upload video below:',
      style: TextStyle(
      fontFamily: 'Museo',
      fontSize: 15,
@@ -110,50 +148,95 @@ SizedBox(
 
 //browse file region
 SizedBox(height:8),//gap
-SizedBox(
-  width: width*0.7,
-  height: height*0.3,
-  child:Card(
-          child: RaisedButton(
-          onPressed: (){}, //Function of the button when press
-    elevation: 3,
-    shape: const RoundedRectangleBorder(
-    borderRadius: BorderRadius.all(Radius.circular(15)),
-        ),
-    color: Colors.white, 
-    child:Padding(
-    //move the text to center vertically
-    padding: EdgeInsets.symmetric(vertical: 100,horizontal: 0),
+    Container( 
+      width: width*0.7,
+      height: height*0.25,
+      child: RaisedButton(
+        onPressed: (){}, //Function of the button when press
+        color: Colors.white,
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+          ),
     child: Text(                              
     'Click to browse file/\ndrag & drop',
     //move the text to center horizontally
     textAlign:TextAlign.center,
      style: TextStyle(
      fontFamily: 'Museo',
+     fontSize: 18,
+     color: Colors.grey[400],
+     fontWeight: FontWeight.bold),
+      ),
+    )
+   ),
+//----------------------------------------------------
+//Text: "Upload data file below"
+SizedBox(height:height*0.05),//gap   
+SizedBox(
+  width: width*0.7,
+  height: 40,
+  child:Card(
+    shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(Radius.circular(15)),
+        ),
+    color: Colors.amber[600],
+    child:Padding(
+    padding: const EdgeInsets.all(7),
+    child: Text(                              
+    '  Upload data file below: ',
+     style: TextStyle(
+     fontFamily: 'Museo',
      fontSize: 15,
-     color: Colors.grey,
+     color: Colors.white,
      fontWeight: FontWeight.bold),
       ),
     ),
-   ),
   ),
 ),
 
-         ],)//column
-        )
-      ],//children
+//browse file region
+SizedBox(height:8),//gap
+    Container( 
+      width: width*0.7,
+      height: height*0.25,
+      child: RaisedButton(
+        onPressed: (){}, //Function of the button when press
+        color: Colors.white,
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+          ),
+    child: Text(                              
+    'Click to browse file/\ndrag & drop',
+    //move the text to center horizontally
+    textAlign:TextAlign.center,
+     style: TextStyle(
+     fontFamily: 'Museo',
+     fontSize: 18,
+     color: Colors.grey[400],
+     fontWeight: FontWeight.bold),
+      ),
     )
-    ),
+   ),
+            ],)//column
+          ),
+         ),
+        ],//children
+       )
+      ),
+  
 //-----------------------------------------------------
+//Orange Upload Button
     Container( 
       height: 50.0,
       width:width*0.8,
       child: RaisedButton(
         onPressed: (){}, //Function of the button when press
-        color: Colors.orange,
+        color: Colors.orange[700],
         shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(40.0),
-          ),
+      ),
       child: Text(
         'Click to upload ',
        style: TextStyle(
@@ -162,14 +245,16 @@ SizedBox(
        fontSize: height * 0.025,
        fontWeight: FontWeight.bold,
         )
-      ), 
-      )
-    ),
+     ), 
+    )
+   ),
+//-----------------------------------------------------     
   ],
-),
-),
-),
- );
-  }
-  
-}
+ ),
+),//Center
+            ],  
+          ),//stack
+        ),
+      );
+    }
+  }  
