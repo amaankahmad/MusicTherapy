16c60ddd60dc906cbc785b6fa6d526ad157326d6
Jemima Hsu <jemimahsu@yahoo.co.uk>
2021-06-02 21:07:00 +0100

    Added safe area

--- musictherapy/lib/ui/pages/adminStartPage.dart
+++ musictherapy/lib/ui/pages/adminStartPage.dart
@@ -26,206 +26,210 @@ class AdminStartPage extends StatelessWidget {
     });
     return Scaffold(
       backgroundColor: honeydew,
-      body: Stack(
-        children: <Widget>[
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
 // --------- BACK BUTTON --------------
-          Positioned(
-            top: 60,
-            left: 40,
-            child: Container(
-              width: 50,
-              height: 50,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => SignIn(),
-                    ),
-                  );
-                },
-                child: Material(
-                  borderRadius: BorderRadius.circular(10000),
-                  shadowColor: orange,
-                  elevation: 3,
-                  child: ClipOval(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        top: 12,
-                        left: 10,
-                        right: 12,
-                        bottom: 12,
+            Positioned(
+              top: 60,
+              left: 40,
+              child: Container(
+                width: 50,
+                height: 50,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => SignIn(),
                       ),
-                      child: Image.asset(
-                        'assets/images/navigation/arrow.jpeg',
+                    );
+                  },
+                  child: Material(
+                    borderRadius: BorderRadius.circular(10000),
+                    shadowColor: orange,
+                    elevation: 3,
+                    child: ClipOval(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 12,
+                          left: 10,
+                          right: 12,
+                          bottom: 12,
+                        ),
+                        child: Image.asset(
+                          'assets/images/navigation/arrow.jpeg',
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
 // --------- BACK BUTTON --------------
 // --------- ERRYTING ELSE ------------
-          Column(children: <Widget>[
+            Column(
+            mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
 // --------- SPACING CONTROL --------------
-            SizedBox(
-              height: height * 0.12,
-            ),
+              SizedBox(
+                height: height * 0.1,
+              ),
 // --------- SPACING CONTROL --------------
 // --------- AVATAR DON -------------------
-            CircleAvatar(
-              radius: 80,
-              backgroundColor: orange,
-              child: CircleAvatar(
-                radius: 65,
-                backgroundImage: AssetImage(
-                  "assets/images/avatars/parrot.jpeg",
+              CircleAvatar(
+                radius: 80,
+                backgroundColor: orange,
+                child: CircleAvatar(
+                  radius: 65,
+                  backgroundImage: AssetImage(
+                    "assets/images/avatars/parrot.jpeg",
+                  ),
                 ),
               ),
-            ),
 // --------- AVATAR DON -------------------
 // --------- SPACING CONTROL --------------
-            SizedBox(
-              height: height * 0.04,
-            ),
+              SizedBox(
+                height: height * 0.05,
+              ),
 // --------- SPACING CONTROL --------------
 // --------- WELCOME TEXT -----------------
-                Center(
-                  child: Container(
-                    child: Text(
-                     'Hello, \n ' + _username,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        //fontFamily: 'Museo',
-                        fontSize: height * 0.06,
-                        color: blue,
-                        fontWeight: FontWeight.w700,
+                  Center(
+                    child: Container(
+                      child: Text(
+                       'Hello,\n' + _username,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          //fontFamily: 'Museo',
+                          fontSize: height * 0.06,
+                          color: blue,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                     ),
                   ),
-                ),
 // --------- SPACING CONTROL --------------
-            SizedBox(
-              height: height * 0.12,
-            ),
+              SizedBox(
+                height: height * 0.12,
+              ),
 // --------- SPACING CONTROL --------------
 // --------- CREATE EXERCISES BUTTON ------
-            Container(
-              width: width * 0.75,
-              height: height * 0.1,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => CreateExercises(),
-                    ),
-                  );
-                },
-                child: Material(
-                  borderRadius: BorderRadius.circular(40),
-                  shadowColor: orange,
-                  color: orange,
-                  elevation: 3,
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      child: Text(
-                        'Create Exercises',
-                        style: TextStyle(
-                          color: white,
-                          //fontFamily: 'Museo',
-                          fontSize: 30,
-                          //fontWeight: FontWeight.w700,
+              Container(
+                width: width * 0.75,
+                height: height * 0.1,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => CreateExercises(),
+                      ),
+                    );
+                  },
+                  child: Material(
+                    borderRadius: BorderRadius.circular(40),
+                    shadowColor: orange,
+                    color: orange,
+                    elevation: 3,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          'Create Exercises',
+                          style: TextStyle(
+                            color: white,
+                            //fontFamily: 'Museo',
+                            fontSize: 30,
+                            //fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
 // --------- CREATE EXERCISES BUTTON ------
 // --------- SPACING CONTROL --------------
-            SizedBox(
-              height: height * 0.04,
-            ),
+              SizedBox(
+                height: height * 0.04,
+              ),
 // --------- SPACING CONTROL --------------
 // --------- MANAGE PLAYERS BUTTON --------
-            Container(
-              width: width * 0.75,
-              height: height * 0.1,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => SelectPlayer(),
-                    ),
-                  );
-                },
-                child: Material(
-                  borderRadius: BorderRadius.circular(40),
-                  shadowColor: orange,
-                  color: orange,
-                  elevation: 3,
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      child: Text(
-                        'Manage Players',
-                        style: TextStyle(
-                          color: white,
-                          //fontFamily: 'Museo',
-                          fontSize: 30,
-                          //fontWeight: FontWeight.w700,
+              Container(
+                width: width * 0.75,
+                height: height * 0.1,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => SelectPlayer(),
+                      ),
+                    );
+                  },
+                  child: Material(
+                    borderRadius: BorderRadius.circular(40),
+                    shadowColor: orange,
+                    color: orange,
+                    elevation: 3,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          'Manage Players',
+                          style: TextStyle(
+                            color: white,
+                            //fontFamily: 'Museo',
+                            fontSize: 30,
+                            //fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
 // --------- MANAGE PLAYERS BUTTON --------
 // --------- SPACING CONTROL --------------
-            SizedBox(
-              height: height * 0.04,
-            ),
+              SizedBox(
+                height: height * 0.04,
+              ),
 // --------- SPACING CONTROL --------------
 // --------- SETTINGS BUTTON --------------
-            Container(
-              width: width * 0.75,
-              height: height * 0.1,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => SettingsPage(),
-                    ),
-                  );
-                },
-                child: Material(
-                  borderRadius: BorderRadius.circular(40),
-                  shadowColor: orange,
-                  color: white,
-                  elevation: 3,
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      child: Text(
-                        'Settings',
-                        style: TextStyle(
-                          color: green,
-                          //fontFamily: 'Museo',
-                          fontSize: 30,
-                          //fontWeight: FontWeight.w700,
+              Container(
+                width: width * 0.75,
+                height: height * 0.1,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => SettingsPage(),
+                      ),
+                    );
+                  },
+                  child: Material(
+                    borderRadius: BorderRadius.circular(40),
+                    shadowColor: orange,
+                    color: white,
+                    elevation: 3,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          'Settings',
+                          style: TextStyle(
+                            color: green,
+                            //fontFamily: 'Museo',
+                            fontSize: 30,
+                            //fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
 // --------- SETTINGS BUTTON --------------
-          ])
-        ],
+            ])
+          ],
+        ),
       ),
     );
   }
