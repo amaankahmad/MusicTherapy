c1e343f987ff1b688a404c47e3bde13b51f7572b
amaankahmad <amaankahmad@gmail.com>
2021-06-02 12:10:00 +0100

    Play page ish

--- bluetooth/example/lib/Device_Screen.dart
+++ bluetooth/example/lib/Device_Screen.dart
@@ -35,11 +35,11 @@ class DeviceScreen extends StatelessWidget {
                 .map(
                   (c) => CharacteristicTile(
                     characteristic: c,
-                    onReadPressed: () => c.read(),
-                    onWritePressed: () async {
-                      await c.write(_getRandomBytes(), withoutResponse: true);
-                      await c.read();
-                    },
+                    // onReadPressed: () => c.read(),
+                    // onWritePressed: () async {
+                    //   await c.write(_getRandomBytes(), withoutResponse: true);
+                    //   await c.read();
+                    // },
                     onNotificationPressed: () async {
                       await c.setNotifyValue(!c.isNotifying);
                       await c.read();
@@ -48,8 +48,8 @@ class DeviceScreen extends StatelessWidget {
                         .map(
                           (d) => DescriptorTile(
                             descriptor: d,
-                            onReadPressed: () => d.read(),
-                            onWritePressed: () => d.write(_getRandomBytes()),
+                            // onReadPressed: () => d.read(),
+                            // onWritePressed: () => d.write(_getRandomBytes()),
                           ),
                         )
                         .toList(),
@@ -103,67 +103,159 @@ class DeviceScreen extends StatelessWidget {
           )
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            // StreamBuilder<BluetoothDeviceState>(
-            //   stream: device.state,
-            //   initialData: BluetoothDeviceState.connecting,
-            //   builder: (c, snapshot) => ListTile(
-            //     leading: (snapshot.data == BluetoothDeviceState.connected)
-            //         ? Icon(Icons.bluetooth_connected)
-            //         : Icon(Icons.bluetooth_disabled),
-            //     title: Text(
-            //         'Device is ${snapshot.data.toString().split('.')[1]}.'),
-            //     subtitle: Text('${device.id}'),
-            //     trailing: StreamBuilder<bool>(
-            //       stream: device.isDiscoveringServices,
-            //       initialData: false,
-            //       builder: (c, snapshot) => IndexedStack(
-            //         index: snapshot.data! ? 1 : 0,
-            //         children: <Widget>[
-            //           IconButton(
-            //             icon: Icon(Icons.refresh),
-            //             onPressed: () => device.discoverServices(),
-            //           ),
-            //           IconButton(
-            //             icon: SizedBox(
-            //               child: CircularProgressIndicator(
-            //                 valueColor: AlwaysStoppedAnimation(Colors.grey),
-            //               ),
-            //               width: 18.0,
-            //               height: 18.0,
-            //             ),
-            //             onPressed: null,
-            //           )
-            //         ],
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // StreamBuilder<int>(
-            //   stream: device.mtu,
-            //   initialData: 0,
-            //   builder: (c, snapshot) => ListTile(
-            //     title: Text('MTU Size'),
-            //     subtitle: Text('${snapshot.data} bytes'),
-            //     trailing: IconButton(
-            //       icon: Icon(Icons.edit),
-            //       onPressed: () => device.requestMtu(223),
-            //     ),
-            //   ),
-            // ),
-            StreamBuilder<List<BluetoothService>>(
-              stream: device.services,
-              initialData: [],
-              builder: (c, snapshot) {
-                return Column(
-                  children: _buildServiceTiles(snapshot.data!),
-                );
-              },
+      body: Column(
+        children: <Widget>[
+          // StreamBuilder<BluetoothDeviceState>(
+          //   stream: device.state,
+          //   initialData: BluetoothDeviceState.connecting,
+          //   builder: (c, snapshot) => ListTile(
+          //     leading: (snapshot.data == BluetoothDeviceState.connected)
+          //         ? Icon(Icons.bluetooth_connected)
+          //         : Icon(Icons.bluetooth_disabled),
+          //     title: Text(
+          //         'Device is ${snapshot.data.toString().split('.')[1]}.'),
+          //     subtitle: Text('${device.id}'),
+          //     trailing: StreamBuilder<bool>(
+          //       stream: device.isDiscoveringServices,
+          //       initialData: false,
+          //       builder: (c, snapshot) => IndexedStack(
+          //         index: snapshot.data! ? 1 : 0,
+          //         children: <Widget>[
+          //           IconButton(
+          //             icon: Icon(Icons.refresh),
+          //             onPressed: () => device.discoverServices(),
+          //           ),
+          //           IconButton(
+          //             icon: SizedBox(
+          //               child: CircularProgressIndicator(
+          //                 valueColor: AlwaysStoppedAnimation(Colors.grey),
+          //               ),
+          //               width: 18.0,
+          //               height: 18.0,
+          //             ),
+          //             onPressed: null,
+          //           )
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // StreamBuilder<int>(
+          //   stream: device.mtu,
+          //   initialData: 0,
+          //   builder: (c, snapshot) => ListTile(
+          //     title: Text('MTU Size'),
+          //     subtitle: Text('${snapshot.data} bytes'),
+          //     trailing: IconButton(
+          //       icon: Icon(Icons.edit),
+          //       onPressed: () => device.requestMtu(223),
+          //     ),
+          //   ),
+          // ),
+// ------------- GROOVE METER ---------------
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+            child: Container(
+              width: 500,
+              height: 10,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40), color: orange),
             ),
-          ],
-        ),
+          ),
+// ------------- MAIN BIT ---------------
+          Center(
+            child: Column(
+              children: [
+// ------------- GIF ---------------
+                Column(
+                  children: <Widget>[
+// ------------- REPS ---------------
+                    StreamBuilder<List<BluetoothService>>(
+                      stream: device.services,
+                      initialData: [],
+                      builder: (c, snapshot) {
+                        return Column(
+                          children: _buildServiceTiles(snapshot.data!),
+                        );
+                      },
+                    ),
+// ------------- TIMER ---------------
+                  ],
+                ),
+              ],
+            ),
+          ),
+//           Padding(
+//             padding: const EdgeInsets.all(10.0),
+//             child: Row(
+//               children: [
+// // ------------- START MUSIC ---------------
+//                 Expanded(
+//                   child: Container(
+//                     child: GestureDetector(
+//                       onTap: () {
+//                         // Music On Tap Function
+//                       },
+//                       child: Material(
+//                         borderRadius: BorderRadius.circular(40),
+//                         shadowColor: orange,
+//                         color: blue,
+//                         elevation: 3,
+//                         child: Center(
+//                           child: Padding(
+//                             padding: const EdgeInsets.symmetric(vertical: 12),
+//                             child: Text(
+//                               'Start Music',
+//                               style: TextStyle(
+//                                 color: white,
+//                                 //fontFamily: 'Museo',
+//                                 fontSize: 24,
+//                                 //fontWeight: FontWeight.w700,
+//                               ),
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(
+//                   width: 20,
+//                 ),
+// // ------------- START COUNTING REPS ---------------
+//                 Expanded(
+//                   child: Container(
+//                     child: GestureDetector(
+//                       onTap: () {
+//                         // Music On Tap Function
+//                       },
+//                       child: Material(
+//                         borderRadius: BorderRadius.circular(40),
+//                         shadowColor: orange,
+//                         color: green,
+//                         elevation: 3,
+//                         child: Center(
+//                           child: Padding(
+//                             padding: const EdgeInsets.symmetric(vertical: 12),
+//                             child: Text(
+//                               'Start Counting',
+//                               style: TextStyle(
+//                                 color: white,
+//                                 //fontFamily: 'Museo',
+//                                 fontSize: 24,
+//                                 //fontWeight: FontWeight.w700,
+//                               ),
+//                             ),
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+        ],
       ),
     );
   }
--- bluetooth/example/lib/widgets.dart
+++ bluetooth/example/lib/widgets.dart
@@ -125,7 +125,12 @@ class ScanResultTile extends StatelessWidget {
 class ServiceTile extends StatelessWidget {
   final BluetoothService service;
   final List<CharacteristicTile> characteristicTiles;
-
+  final orange = const Color(0xFFF57E00);
+  final green = const Color(0xFF04A489);
+  final blue = const Color(0xFF1E325C);
+  final white = const Color(0xFFFFFBF2);
+  final yellow = const Color(0xFFFFC247);
+  final honeydew = const Color(0xFFF1FAEE);
   const ServiceTile(
       {Key? key, required this.service, required this.characteristicTiles})
       : super(key: key);
@@ -165,6 +170,12 @@ class CharacteristicTile extends StatelessWidget {
   final VoidCallback? onReadPressed;
   final VoidCallback? onWritePressed;
   final VoidCallback? onNotificationPressed;
+  final orange = const Color(0xFFF57E00);
+  final green = const Color(0xFF04A489);
+  final blue = const Color(0xFF1E325C);
+  final white = const Color(0xFFFFFBF2);
+  final yellow = const Color(0xFFFFC247);
+  final honeydew = const Color(0xFFF1FAEE);
 
   const CharacteristicTile(
       {Key? key,
@@ -188,53 +199,146 @@ class CharacteristicTile extends StatelessWidget {
         } else {
           reps = 0;
         }
-        return ExpansionTile(
-          title: ListTile(
-            title: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Rep Counter'),
-                // Text(
-                //     '0x${characteristic.uuid.toString().toUpperCase().substring(4, 8)}',
-                //     style: Theme.of(context).textTheme.body1?.copyWith(
-                //         color: Theme.of(context).textTheme.caption?.color))
+        return Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 200,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Rep Counter:',
+                      style: TextStyle(
+                        color: blue,
+                        fontSize: 20,
+                      ),
+                    ),
+                    // Text(
+                    //     '0x${characteristic.uuid.toString().toUpperCase().substring(4, 8)}',
+                    //     style: Theme.of(context).textTheme.body1?.copyWith(
+                    //         color: Theme.of(context).textTheme.caption?.color))
+                    Container(
+                      child: Text(
+                        reps.toString(),
+                        style: TextStyle(
+                          color: blue,
+                          fontSize: 125,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            subtitle: Text(reps.toString()),
-            contentPadding: EdgeInsets.all(0.0),
-          ),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              // IconButton(
-              //   icon: Icon(
-              //     Icons.file_download,
-              //     color: Theme.of(context).iconTheme.color?.withOpacity(0.5),
-              //   ),
-              //   onPressed: onReadPressed,
-              // ),
-              // IconButton(
-              //   icon: Icon(Icons.file_upload,
-              //       color: Theme.of(context).iconTheme.color?.withOpacity(0.5)),
-              //   onPressed: onWritePressed,
-              // ),
-              IconButton(
-                icon: Icon(
-                    characteristic.isNotifying
-                        ? Icons.sync_disabled
-                        : Icons.sync,
-                    color: Theme.of(context).iconTheme.color?.withOpacity(0.5)),
-                // onPressed: () => {
-                //   onNotificationPressed,
-                //   // Music play
-                // },
-                onPressed: onNotificationPressed,
-              )
-            ],
-          ),
-          children: descriptorTiles,
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+// ------------- START MUSIC ---------------
+                  Expanded(
+                    child: Container(
+                      child: GestureDetector(
+                        onTap: () {
+                          // Music On Tap Function
+                        },
+                        child: Material(
+                          borderRadius: BorderRadius.circular(40),
+                          shadowColor: orange,
+                          color: blue,
+                          elevation: 3,
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 12),
+                              child: Text(
+                                'Start Music',
+                                style: TextStyle(
+                                  color: white,
+                                  //fontFamily: 'Museo',
+                                  fontSize: 20,
+                                  //fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+// ------------- START COUNTING REPS ---------------
+                  Expanded(
+                    child: Container(
+                      child: GestureDetector(
+                        onTap: onNotificationPressed,
+                        child: Material(
+                          borderRadius: BorderRadius.circular(40),
+                          shadowColor: orange,
+                          color: green,
+                          elevation: 3,
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 12),
+                              child: Text(
+                                'Start Counting',
+                                style: TextStyle(
+                                  color: white,
+                                  //fontFamily: 'Museo',
+                                  fontSize: 20,
+                                  //fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         );
+        trailing:
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // IconButton(
+            //   icon: Icon(
+            //     Icons.file_download,
+            //     color: Theme.of(context).iconTheme.color?.withOpacity(0.5),
+            //   ),
+            //   onPressed: onReadPressed,
+            // ),
+            // IconButton(
+            //   icon: Icon(Icons.file_upload,
+            //       color: Theme.of(context).iconTheme.color?.withOpacity(0.5)),
+            //   onPressed: onWritePressed,
+            // ),
+            IconButton(
+              icon: Icon(
+                  characteristic.isNotifying ? Icons.sync_disabled : Icons.sync,
+                  color: Theme.of(context).iconTheme.color?.withOpacity(0.5)),
+              // onPressed: () => {
+              //   onNotificationPressed,
+              //   // Music play
+              // },
+              onPressed: onNotificationPressed,
+            )
+          ],
+        );
+        //   children: descriptorTiles,
+        // );
       },
     );
   }
@@ -244,6 +348,12 @@ class DescriptorTile extends StatelessWidget {
   final BluetoothDescriptor descriptor;
   final VoidCallback? onReadPressed;
   final VoidCallback? onWritePressed;
+  final orange = const Color(0xFFF57E00);
+  final green = const Color(0xFF04A489);
+  final blue = const Color(0xFF1E325C);
+  final white = const Color(0xFFFFFBF2);
+  final yellow = const Color(0xFFFFC247);
+  final honeydew = const Color(0xFFF1FAEE);
 
   const DescriptorTile(
       {Key? key,
