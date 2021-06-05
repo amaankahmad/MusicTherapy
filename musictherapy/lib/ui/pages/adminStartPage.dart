650a3cd09bc9d8f514bbb9e43ba2b234c80158d1
Jemima Hsu <jemimahsu@yahoo.co.uk>
2021-06-05 22:14:30 +0100

    Added Stateful Widget and Expanded buttons

--- musictherapy/lib/ui/pages/adminStartPage.dart
+++ musictherapy/lib/ui/pages/adminStartPage.dart
@@ -8,7 +8,12 @@ import 'package:musictherapy/ui/pages/selectPlayer.dart';
 import 'package:musictherapy/ui/pages/settings.dart';
 import 'package:musictherapy/ui/pages/signInPage.dart';
 
-class AdminStartPage extends StatelessWidget {
+class AdminStartPage extends StatefulWidget {
+  @override
+  AdminStartState createState() => AdminStartState();
+}
+
+class AdminStartState extends State<AdminStartPage> {
   String _username;
   final cUser = FirebaseAuth.instance.currentUser;
   @override
@@ -21,9 +26,27 @@ class AdminStartPage extends StatelessWidget {
     final white = const Color(0xFFFFFBF2);
     final yellow = const Color(0xFFFFC247);
     final honeydew = const Color(0xFFF1FAEE);
-    FirebaseFirestore.instance.collection("user_info").doc(cUser.uid).get().then((value) {
-    _username = value.data()["username"];
+
+    /*FirebaseFirestore.instance
+        .collection("user_info")
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        _username = doc["username"];
+      });
+    });*/
+
+    FirebaseFirestore.instance
+        .collection("user_info")
+        .doc(cUser.uid)
+        .get()
+        .then((value) {
+      _username = value.data()["username"];
     });
+
+    /*FirebaseFirestore.instance.collection("user_info").doc(cUser.uid).get().then((value) {
+    _username = value.data()["username"];
+    });*/
     return Scaffold(
       backgroundColor: honeydew,
       body: SafeArea(
@@ -67,9 +90,7 @@ class AdminStartPage extends StatelessWidget {
             ),
 // --------- BACK BUTTON --------------
 // --------- ERRYTING ELSE ------------
-            Column(
-            mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
+            Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
 // --------- SPACING CONTROL --------------
               SizedBox(
                 height: height * 0.1,
@@ -93,52 +114,54 @@ class AdminStartPage extends StatelessWidget {
               ),
 // --------- SPACING CONTROL --------------
 // --------- WELCOME TEXT -----------------
-                  Center(
-                    child: Container(
-                      child: Text(
-                       'Hello,\n' + _username,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          //fontFamily: 'Museo',
-                          fontSize: height * 0.06,
-                          color: blue,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
+              Center(
+                child: Container(
+                  child: Text(
+                    'Hello,\n $_username!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      //fontFamily: 'Museo',
+                      fontSize: height * 0.06,
+                      color: blue,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
+                ),
+              ),
 // --------- SPACING CONTROL --------------
               SizedBox(
-                height: height * 0.12,
+                height: height * 0.05,
               ),
 // --------- SPACING CONTROL --------------
 // --------- CREATE EXERCISES BUTTON ------
-              Container(
-                width: width * 0.75,
-                height: height * 0.1,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => CreateExercises(),
-                      ),
-                    );
-                  },
-                  child: Material(
-                    borderRadius: BorderRadius.circular(40),
-                    shadowColor: orange,
-                    color: orange,
-                    elevation: 3,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                        child: Text(
-                          'Create Exercises',
-                          style: TextStyle(
-                            color: white,
-                            //fontFamily: 'Museo',
-                            fontSize: 30,
-                            //fontWeight: FontWeight.w700,
+              Expanded(
+                child: Container(
+                  width: width * 0.75,
+                  height: height * 0.1,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => CreateExercises(),
+                        ),
+                      );
+                    },
+                    child: Material(
+                      borderRadius: BorderRadius.circular(40),
+                      shadowColor: orange,
+                      color: orange,
+                      elevation: 3,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          child: Text(
+                            'Create Exercises',
+                            style: TextStyle(
+                              color: white,
+                              //fontFamily: 'Museo',
+                              fontSize: 30,
+                              //fontWeight: FontWeight.w700,
+                            ),
                           ),
                         ),
                       ),
@@ -153,32 +176,34 @@ class AdminStartPage extends StatelessWidget {
               ),
 // --------- SPACING CONTROL --------------
 // --------- MANAGE PLAYERS BUTTON --------
-              Container(
-                width: width * 0.75,
-                height: height * 0.1,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => SelectPlayer(),
-                      ),
-                    );
-                  },
-                  child: Material(
-                    borderRadius: BorderRadius.circular(40),
-                    shadowColor: orange,
-                    color: orange,
-                    elevation: 3,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                        child: Text(
-                          'Manage Players',
-                          style: TextStyle(
-                            color: white,
-                            //fontFamily: 'Museo',
-                            fontSize: 30,
-                            //fontWeight: FontWeight.w700,
+              Expanded(
+                child: Container(
+                  width: width * 0.75,
+                  height: height * 0.1,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => SelectPlayer(),
+                        ),
+                      );
+                    },
+                    child: Material(
+                      borderRadius: BorderRadius.circular(40),
+                      shadowColor: orange,
+                      color: orange,
+                      elevation: 3,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          child: Text(
+                            'Manage Players',
+                            style: TextStyle(
+                              color: white,
+                              //fontFamily: 'Museo',
+                              fontSize: 30,
+                              //fontWeight: FontWeight.w700,
+                            ),
                           ),
                         ),
                       ),
@@ -193,38 +218,44 @@ class AdminStartPage extends StatelessWidget {
               ),
 // --------- SPACING CONTROL --------------
 // --------- SETTINGS BUTTON --------------
-              Container(
-                width: width * 0.75,
-                height: height * 0.1,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => SettingsPage(),
-                      ),
-                    );
-                  },
-                  child: Material(
-                    borderRadius: BorderRadius.circular(40),
-                    shadowColor: orange,
-                    color: white,
-                    elevation: 3,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12),
-                        child: Text(
-                          'Settings',
-                          style: TextStyle(
-                            color: green,
-                            //fontFamily: 'Museo',
-                            fontSize: 30,
-                            //fontWeight: FontWeight.w700,
+              Expanded(
+                child: Container(
+                  width: width * 0.75,
+                  height: height * 0.1,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => SettingsPage(),
+                        ),
+                      );
+                    },
+                    child: Material(
+                      borderRadius: BorderRadius.circular(40),
+                      shadowColor: orange,
+                      color: white,
+                      elevation: 3,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          child: Text(
+                            'Settings',
+                            style: TextStyle(
+                              color: green,
+                              //fontFamily: 'Museo',
+                              fontSize: 30,
+                              //fontWeight: FontWeight.w700,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
                 ),
+              ),
+
+              SizedBox(
+                height: height * 0.1,
               ),
 // --------- SETTINGS BUTTON --------------
             ])
