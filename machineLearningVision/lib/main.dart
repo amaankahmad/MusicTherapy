f2a8cbaf21a2c8ff4fb71bc0818abd70b57a3cc1
amaankahmad <amaankahmad@gmail.com>
2021-06-07 12:37:47 +0100

    ML stuff

--- machineLearningVision/lib/main.dart
+++ machineLearningVision/lib/main.dart
@@ -20,7 +20,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'tflite real-time detection',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
